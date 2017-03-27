a2dcdd4bd94035536f8efa05f41c587e2e0c18d3
Mathieu CHEMAKH <mchemakh@e3r11p16.42.fr>
2017-03-27 04:22:27 +0200

    ft_printf norme 10 correctif 2

--- srcs/ft_ldigitflag_n.c
+++ srcs/ft_ldigitflag_n.c
@@ -6,13 +6,13 @@
 /*   By: mchemakh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/27 04:11:18 by mchemakh          #+#    #+#             */
-/*   Updated: 2017/03/27 04:20:01 by mchemakh         ###   ########.fr       */
+/*   Updated: 2017/03/27 04:22:07 by mchemakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_ldigit_nnnn(t_flags *list, int *size, int *i, char ** newarg)
+void	ft_ldigit_nnnn(t_flags *list, int *size, int *i, char **newarg)
 {
 	(*newarg) = NULL;
 	(*i) = 0;
