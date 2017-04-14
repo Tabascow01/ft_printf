02b9717f58f0f78d42d8570fb3eed10f34b1fdb2
Tabascow <tabascow@tabascows-mac-mini.home>
2017-04-14 06:37:38 +0200

    maj

--- srcs/ft_digitflag.c
+++ srcs/ft_digitflag.c
@@ -83,6 +83,7 @@ void			ft_digitflag(t_flags *list)
 		while (lst->i < (list->dig2 - lst->size))
 			newarg[lst->i++] = ' ';
 		ft_digit_nn(list, &lst->i, &newarg, &lst->tmp);
+		ft_clear_precs(lst);
 	}
 	else
 	{
@@ -94,6 +95,5 @@ void			ft_digitflag(t_flags *list)
 			newarg = ft_strnew(list->dig1 + list->dig2);
 		ft_precs(list, newarg, lst);
 	}
-	ft_clear_precs(lst);
 	free(lst);
 }
