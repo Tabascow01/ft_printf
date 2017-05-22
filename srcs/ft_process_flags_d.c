9d08caf319c33d89c3a409a7d941c88312959f14
Mathieu CHEMAKH <mchemakh@e3r8p7.42.fr>
2017-05-22 04:32:48 +0200

    ft_printf update flag_d

--- srcs/ft_process_flags_d.c
+++ srcs/ft_process_flags_d.c
@@ -6,7 +6,7 @@
 /*   By: mchemakh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/21 23:39:44 by mchemakh          #+#    #+#             */
-/*   Updated: 2017/05/22 04:24:05 by mchemakh         ###   ########.fr       */
+/*   Updated: 2017/05/22 04:32:36 by mchemakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,11 @@ void	ft_process_flags_d(t_flags *list)
 		ft_zeroflag(list);
 	if (list->sign > 0)
 		ft_signflag(list);
-	if ((int)ft_strlen(list->digit) > 0 && list->left == 0)
+	if ((int)ft_strlen(list->digit) > 0 && list->left == 0
+			&& list->precision > 0)
 		ft_digitflag(list);
-	else if ((int)ft_strlen(list->digit) > 0 && list->left > 0)
+	else if ((int)ft_strlen(list->digit) > 0 && list->left > 0
+			&& list->precision > 0)
 		ft_ldigitflag(list);
 	else if (list->space > 0 && list->precision == 0 && list->sign == 0)
 		ft_spaceflag(list);
